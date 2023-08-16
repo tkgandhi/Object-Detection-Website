f20965904d3792a4d49ecb9baaa840562a614df5
tkgandhi <109563744+tkgandhi@users.noreply.github.com>
2023-08-16 12:02:55 -0400

    Add files via upload

--- glasses.js
+++ glasses.js
@@ -1,4 +1,5 @@
 img="";
+status="";
 
 function preload()
 {
@@ -8,15 +9,29 @@ function preload()
 function setup()
 {
     canvas=createCanvas(640,420);
-    canvas.center();
+    canvas.position(400,200);
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects" 
+}
+
+function modelLoaded() 
+{
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
 }
 
 function draw()
 {
-    image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,450,350);    
+    image(img, 0, 0, 640, 420);    
 }
--- pillow.js
+++ pillow.js
@@ -1,22 +1,38 @@
 img="";
+status="";
 
 function preload()
 {
-    img=loadImage("pillow.jpeg");
+    img=loadImage("pillow.jpg");
 }
 
 function setup()
 {
     canvas=createCanvas(640,420);
-    canvas.center();
+    canvas.position(400,200);
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects" 
+}
+
+function modelLoaded() 
+{
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
 }
 
 function draw()
 {
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,450,350);    
+       
 }
